8ef33cb8e62f28d69a7e0fd7467fceb634dd7d8c
ZERGLING-HIVE\Hive <death1681@yandex.ru>
2018-03-11 13:58:59 +0300

    - заготовка под окно для тестов шрифтов - проверяю, что будет раньше в MonoInstaller-е

--- Pathfinder-hero-editor/Assets/Scripts/Installers/MainInstaller.cs
+++ Pathfinder-hero-editor/Assets/Scripts/Installers/MainInstaller.cs
@@ -7,6 +7,11 @@ public class MainInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
-        
+        Debug.Log("MainInstaller InstallBindings");
+    }
+
+    public override void Start()
+    {
+        Debug.Log("MainInstaller Start");
     }
 }
