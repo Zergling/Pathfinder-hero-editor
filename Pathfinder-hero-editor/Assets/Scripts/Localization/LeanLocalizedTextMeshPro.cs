09f98b5d62c1d8fa5bc8d9cd696e1bbe107d6c28
Hive <death1681@yandex.ru>
2018-04-15 19:46:07 +0300

    - написал скрипт для автолокализации текстовых полей текст меш про

---
+++ Pathfinder-hero-editor/Assets/Scripts/Localization/LeanLocalizedTextMeshPro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Localization;
+using TMPro;
+
+[ExecuteInEditMode]
+public class LeanLocalizedTextMeshPro : LeanLocalizedBehaviour 
+{
+	public override void UpdateTranslation(LeanTranslation translation)
+	{
+		if (translation == null)
+			return;
+		
+		TMP_Text text = GetComponent<TMP_Text>();
+		if (text == null)
+			return;
+		
+		text.text = translation.Text;
+	}
+}
